84cb6206fcfc7942dbfb62ef2e6b90f174f23f75
nadhell <nadhell@live.com>
2023-05-19 10:01:24 +0200

    added button with no effect

--- screens/CreateDeckScreen.js
+++ screens/CreateDeckScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { TextInput } from "react-native-paper";
 
 export default function CreateDeckScreen() {
@@ -20,6 +20,13 @@ export default function CreateDeckScreen() {
         placeholder="Enter what you would like to learn and remember"
         onChangeText={(newText) => setDefinition(newText)}
         multiline={true}
+      />
+      <Button
+      // TODO : choose between onPress or onClick a Submit event
+      // this should save the card in the phone local storage 
+        title="Save Card"
+        color="#841584"
+        accessibilityLabel="Button to save your card in your local storage button"
       />
     </View>
   );
