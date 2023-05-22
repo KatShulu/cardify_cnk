49666001610a476b63da4f42360d4c35a1215c7e
nadhell <nadhell@live.com>
2023-05-22 15:03:17 +0200

    added a keyboard dismiss to dismiss keyboard when touching in an empty area

--- screens/CreateDeckScreen.js
+++ screens/CreateDeckScreen.js
@@ -1,43 +1,66 @@
 import React, { useState } from "react";
-import { Text, View, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+} from "react-native";
 import { TextInput } from "react-native-paper";
-import * as SecureStore from 'expo-secure-store';
-
+import * as SecureStore from "expo-secure-store";
 
 export default function CreateDeckScreen() {
   const [notion, setNotion] = useState("");
   const [definition, setDefinition] = useState("");
-  return (
-    <View>
-      <Text>🎴 Now create your own Cards!</Text>
-      <TextInput
-        label="Notion"
-        value={notion}
-        placeholder="Enter your word or notion here"
-        onChangeText={(newText) => setNotion(newText)}
-      />
-      <TextInput
-        label="Definition"
-        value={definition}
-        placeholder="Enter what you would like to learn and remember"
-        onChangeText={(newText) => setDefinition(newText)}
-        multiline={true}
-      />
-      <Button
-        title="Save Card"
-        color="#841584"
-        accessibilityLabel="Button to save your card in your local storage button"
-        onPress={() => {
-          saveCard(notion, definition);
-          //TODO: check what these functions do
-          // onChangeKey('Your key here');
-          // onChangeValue('Your value here');
-        }}
-      />
-    </View>
-  );
-}
 
-async function saveCard(key, value) {
-  await SecureStore.setItemAsync(key, value);
+  const saveCard = async (key, value) => {
+    await SecureStore.setItemAsync(key, value);
+  };
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : null}
+        enabled
+      >
+        <View>
+          <Text>🎴 Now create your own Cards!</Text>
+
+          <TextInput
+            label="Notion"
+            value={notion}
+            placeholder="Enter your word or notion here"
+            onChangeText={(newText) => setNotion(newText)}
+          />
+          <TextInput
+            label="Definition"
+            value={definition}
+            placeholder="Enter what you would like to learn and remember"
+            onChangeText={(newText) => setDefinition(newText)}
+            multiline={true}
+          />
+
+          <Button
+            title="Save Card"
+            color="#841584"
+            accessibilityLabel="Button to save your card in your local storage button"
+            onPress={() => {
+              saveCard(notion, definition);
+              Keyboard.dismiss();
+              //TODO: check what these functions do and implement local storage
+              // onChangeKey('Your key here');
+              // onChangeValue('Your value here');
+            }}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
 }
