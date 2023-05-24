76df3d05abf93a5ee83dc14a4fc965b46429540d
Corentin MONVILLERS <c.monvillers@live.fr>
2023-05-24 16:18:06 +0200

    collectionCard

--- screens/CollectionsScreen.js
+++ screens/CollectionsScreen.js
@@ -1,48 +1,107 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Title } from "react-native-paper";
 
 export default function CollectionsScreen() {
   return (
-    <View>
-      <View style={styles.box}>
-        <Title style={styles.title}>Recent Decks</Title>
-        <Text style={styles.text}>Placeholder for recent decks</Text>
+    <View style={styles.container}>
+      <View style={styles.column}>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #1</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #2</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #3</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
-      <View  style={styles.box}>
-        <Title style={styles.title}>My Collections</Title>
-        <Text style={styles.text}>Placeholder for Your Collections</Text>
-      </View>
-      <View  style={styles.box}>
-        <Title style={styles.title}>Public Collections</Title>
-        <Text style={styles.text}>Placeholder for Public Collections</Text>
+      <View style={styles.column}>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #4</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #5</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.box}>
+            <Title style={styles.title}>Name Deck #6</Title>
+            <Text style={styles.text} numberOfLines={2}>
+              Description of Deck
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  box: {
+  container: {
+    flex: 1,
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    padding: 10,
+  },
+  column: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  box: {
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 10,
     paddingVertical: 5,
     backgroundColor: "#087E8A",
     borderColor: "#F5F5F5",
     borderWidth: 2,
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    overflow: "hidden",
   },
   title: {
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#F5F5F5"
+    color: "#F5F5F5",
   },
   text: {
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#F5F5F5"
-  }
+    color: "#F5F5F5",
+    textAlign: "center",
+  },
 });
