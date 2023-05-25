57c29e5a08c2947cf9ef96c61bc0afd3e9a98d5e
Corentin MONVILLERS <c.monvillers@live.fr>
2023-05-25 10:13:08 +0200

    displayDeck and CSS

--- screens/CollectionsScreen.js
+++ screens/CollectionsScreen.js
@@ -1,40 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Title } from "react-native-paper";
-import {getDeckFiles} from "../services/DeckLocalStorage";
+import { getDeckFilesNames } from "../services/DeckLocalStorage";
 
 export default function CollectionsScreen() {
-
   const [decks, setDecks] = useState([]);
 
   useEffect(() => {
     // Récupérer les données des decks au montage du composant
     const fetchData = async () => {
-      const data = await getDeckFiles();
-      console.log(data)
+      const data = await getDeckFilesNames();
       setDecks(data);
     };
 
     fetchData();
   }, []);
 
-  
-  return (
-    <View style={styles.container}>
-      <View style={styles.column}>
-        {decks.map((deck, index) => (
-          <TouchableOpacity key={index}>
-            <View style={styles.box}>
-              <Title style={styles.title}>{deck.name}</Title>
-              <Text style={styles.text} numberOfLines={2}>
-                {deck.description}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        ))}
+  const withoutExtension = (filename) => {
+    return filename.replace(".json", "");
+  };
+
+  const renderDecks = () => {
+    const halfLength = Math.ceil(decks.length / 2);
+    const firstHalf = decks.slice(0, halfLength);
+    const secondHalf = decks.slice(halfLength);
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.column}>
+          {firstHalf.map((deck, index) => (
+            <TouchableOpacity key={index}>
+              <View style={styles.box}>
+                <Title style={styles.title}>{withoutExtension(deck)}</Title>
+                <Text style={styles.text} numberOfLines={2}></Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={styles.column}>
+          {secondHalf.map((deck, index) => (
+            <TouchableOpacity key={index}>
+              <View style={styles.box}>
+                <Title style={styles.title}>{withoutExtension(deck)}</Title>
+                <Text style={styles.text} numberOfLines={2}></Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
       </View>
-    </View>
-  );
+    );
+  };
+
+  return renderDecks();
 }
 
 const styles = StyleSheet.create({
@@ -58,12 +75,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     backgroundColor: "#087E8A",
-    borderColor: "#F5F5F5",
+    borderColor: "#3C3C3C",
     borderWidth: 2,
-    width: "100%",
-    height: 400,
+    width: 180,
+    height: 180,
     marginBottom: 10,
     overflow: "hidden",
+    shadowColor: "black",
+    elevation: 8,
   },
   title: {
     justifyContent: "center",
