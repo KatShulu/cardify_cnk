655910acf89cfb6a7c86df560efc13cde1b139df
Corentin MONVILLERS <c.monvillers@live.fr>
2023-05-25 15:41:40 +0200

    modal open/close

--- screens/CollectionsScreen.js
+++ screens/CollectionsScreen.js
@@ -4,14 +4,15 @@ import {
   View,
   Text,
   TouchableOpacity,
-  ScrollView,
   FlatList,
+  Modal,
 } from "react-native";
-import { Title } from "react-native-paper";
+import { IconButton, Title } from "react-native-paper";
 import { getDeckFilesNames } from "../services/DeckLocalStorage";
 
 export default function CollectionsScreen() {
   const [decks, setDecks] = useState([]);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     // Récupérer les données des decks au montage du composant
@@ -27,8 +28,15 @@ export default function CollectionsScreen() {
     return filename.replace(".json", "");
   };
 
+  const openModal = () => {
+    setModalVisible(true);
+  };
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   const renderDeckItem = ({ item }) => (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={openModal}>
       <View style={styles.box}>
         <Title style={styles.title}>{withoutExtension(item)}</Title>
         <Text style={styles.text} numberOfLines={2}></Text>
@@ -38,15 +46,25 @@ export default function CollectionsScreen() {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <FlatList
-          data={decks}
-          renderItem={renderDeckItem}
-          keyExtractor={(item, index) => index.toString()}
-          numColumns={2}
-          columnWrapperStyle={styles.column}
-        />
-      </ScrollView>
+      <FlatList
+        data={decks}
+        renderItem={renderDeckItem}
+        keyExtractor={(index) => index.toString()}
+        numColumns={2}
+        columnWrapperStyle={styles.column}
+      />
+      <Modal visible={isModalVisible} onRequestClose={closeModal}>
+        <View style={styles.modalContainer}>
+          <IconButton
+            icon="close"
+            iconColor="#000"
+            accessibilityLabel="Delete"
+            style={styles.deleteButton}
+            onPress={closeModal}
+          ></IconButton>
+          <Text style={styles.list}>"Bonjour"</Text>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -61,6 +79,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 10,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   column: {
     justifyContent: "space-between",
     alignItems: "center",
@@ -72,7 +95,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    width: 180, 
+    width: 180,
     height: 200,
     marginBottom: 10,
     overflow: "hidden",
@@ -95,5 +118,18 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     color: "#F5F5F5",
     textAlign: "center",
+  },
+  list: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#000",
+    textAlign: "center",
+  },
+  deleteButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
   },
 });
